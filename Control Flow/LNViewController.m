4021f3f1fa424085dcf2a78eb17ebf56f061f5e0
Gonzalo6282 <gonzalo6282@gmail.com>
2014-11-30 11:59:53 -0800

    Control Flow

--- Control Flow/LNViewController.m
+++ Control Flow/LNViewController.m
@@ -18,6 +18,27 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    int truckSpeed = 45;
+    int lamboSpeed = 120;
+    int carSpeed = 80;
+    int mySpeed = carSpeed;
+    if (mySpeed < 70){
+        NSLog(@"Keep Cruising");
+    }
+    else if (mySpeed > 55 && mySpeed < 90){
+        NSLog(@"Cruising down highway 66");
+    }
+    else{
+        NSLog(@"Slow Down");
+    }
+    BOOL isTelevisionOn = YES;
+    if (isTelevisionOn == YES) {
+        NSLog(@"We shold take a break and do some coding");
+    }
+    else{
+        NSLog(@"Great job keep up the hard work");
+    }
 }
 
 - (void)didReceiveMemoryWarning
